6aac0ee69a76cac5ba9446b87a388a3b7f6a58ab
adbenitez <asieldbenitez@gmail.com>
2021-08-20 21:20:46 -0400

    improve scripts/web_search.py use lite version of DuckDuckGo, quote-reply message

--- scripts/web_search.py
+++ scripts/web_search.py
@@ -11,21 +11,21 @@ def search_filter(bot: DeltaBot, message: Message, replies: Replies) -> None:
     """Send me any text in private to search in the web."""
     if not replies.has_replies() and not message.chat.is_group() and message.text:
         text, html = _search(bot.self_contact.addr, message.text)
-        replies.add(text=text, html=html)
+        replies.add(text=text or "Search results", html=html, quote=message)
 
 
 @simplebot.command
-def search(bot: DeltaBot, payload: str, replies: Replies) -> None:
+def search(bot: DeltaBot, payload: str, message: Message, replies: Replies) -> None:
     """Search the web.
 
     Example:
     /search download Delta Chat for GNU/Linux
     """
     text, html = _search(bot.self_contact.addr, payload)
-    replies.add(text=text, html=html)
+    replies.add(text=text or "Search results", html=html, quote=message)
 
 
 def _search(bot_addr: str, query: str) -> tuple:
-    with session.get(f"https://duckduckgo.com/html?q={quote_plus(query)}") as resp:
+    with session.get(f"https://duckduckgo.com/lite?q={quote_plus(query)}") as resp:
         resp.raise_for_status()
         return prepare_html(bot_addr, resp.url, resp.text)
