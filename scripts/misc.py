333422175ea56ec304a9e4b6dbdfa58c4ecc5dd0
adbenitez <asieldbenitez@gmail.com>
2021-08-21 02:15:06 -0400

    update scripts/misc.py,  add more joke sites to /chiste command

--- scripts/misc.py
+++ scripts/misc.py
@@ -1,8 +1,11 @@
 """Miscellaneous small commands and filters."""
 
 import io
+import random
 from urllib.parse import unquote_plus
 
+import bs4
+import requests
 import simplebot
 
 
@@ -25,93 +28,106 @@ def filter_messages(message, bot) -> None:
             bot.account.qr_setup_contact(message.text)
 
 
-try:
-    import requests
-
-    @simplebot.command
-    def chiste(payload, replies) -> None:
-        """Envía un chiste al azar."""
-        import bs4
-        with requests.get("http://www.chistes.com/ChisteAlAzar.asp?n=1") as resp:
-            soup = bs4.BeautifulSoup(resp.text, "html.parser")
-            joke = soup.find("div", class_="chiste").get_text()
-            replies.add(text=joke)
-
-    @simplebot.command
-    def insult(payload, message, replies) -> None:
-        """insult quoted message."""
-        with requests.get(
-            f"https://evilinsult.com/generate_insult.php?lang={payload}&type=json"
-        ) as resp:
-            replies.add(text=resp.json()["insult"], quote=message.quote)
-
-    @simplebot.command
-    def advice(replies) -> None:
-        """get random advice."""
-        with requests.get("https://api.adviceslip.com/advice") as resp:
-            replies.add(text=resp.json()["slip"]["advice"])
-
-    @simplebot.command
-    def chuckjoke(replies) -> None:
-        """get random Chuck Norris joke."""
-        with requests.get(
-            "http://api.icndb.com/jokes/random?escape=javascript"
-        ) as resp:
-            replies.add(text=resp.json()["value"]["joke"])
-
-    @simplebot.command
-    def joke(payload, replies) -> None:
-        """get random joke."""
-        with requests.get(
-            f"https://v2.jokeapi.dev/joke/Any?format=txt&contains={payload}"
-        ) as resp:
-            replies.add(text=resp.text)
-
-    @simplebot.command
-    def dadjoke(replies) -> None:
-        """get random dad joke."""
-        headers = {
-            "User-Agent": "SimpleBot (https://github.com/simplebot-org/simplebot)",
-            "Accept": "text/plain",
-        }
-        with requests.get("https://icanhazdadjoke.com/", headers=headers) as resp:
-            replies.add(text=resp.text)
+@simplebot.command
+def chiste(replies) -> None:
+    """Envía un chiste al azar."""
+    replies.add(text=random.choice((_chistes, _todo_chistes, _elclubdeloschistes))())
 
 
-except ImportError as ex:
-    print("WARNING:", ex)
+def _chistes() -> str:
+    with requests.get("http://www.chistes.com/ChisteAlAzar.asp?n=1") as resp:
+        soup = bs4.BeautifulSoup(resp.text, "html.parser")
+    return _soup2text(soup.find(class_="chiste")) + "\n\nFuente: http://www.chistes.com"
 
 
-try:
+def _todo_chistes() -> str:
+    with requests.get("http://todo-chistes.com/chistes-al-azar") as resp:
+        soup = bs4.BeautifulSoup(resp.text, "html.parser")
+    return (
+        _soup2text(soup.find(class_="field-chiste"))
+        + "\n\nFuente: http://todo-chistes.com"
+    )
+
+
+def _elclubdeloschistes() -> str:
+    with requests.get("https://elclubdeloschistes.com/azar.php") as resp:
+        soup = bs4.BeautifulSoup(resp.text, "html.parser")
+    soup.b.extract()
+    for tag in soup("a"):
+        tag.extract()
+    joke = _soup2text(soup.find(class_="texto"))
+    return joke[: joke.rfind("\n")] + "\n\nFuente: https://elclubdeloschistes.com"
+
+
+def _soup2text(soup: bs4.BeautifulSoup) -> str:
+    for tag in soup("br"):
+        tag.replace_with("\n")
+    return "\n".join(soup.get_text().split("\n")).strip()
+
+
+@simplebot.command
+def insult(payload, message, replies) -> None:
+    """insult quoted message."""
+    with requests.get(
+        f"https://evilinsult.com/generate_insult.php?lang={payload}&type=json"
+    ) as resp:
+        replies.add(text=resp.json()["insult"], quote=message.quote)
+
+
+@simplebot.command
+def advice(replies) -> None:
+    """get random advice."""
+    with requests.get("https://api.adviceslip.com/advice") as resp:
+        replies.add(text=resp.json()["slip"]["advice"])
+
+
+@simplebot.command
+def chuckjoke(replies) -> None:
+    """get random Chuck Norris joke."""
+    with requests.get("http://api.icndb.com/jokes/random?escape=javascript") as resp:
+        replies.add(text=resp.json()["value"]["joke"])
+
+
+@simplebot.command
+def joke(payload, replies) -> None:
+    """get random joke."""
+    with requests.get(
+        f"https://v2.jokeapi.dev/joke/Any?format=txt&contains={payload}"
+    ) as resp:
+        replies.add(text=resp.text)
+
+
+@simplebot.command
+def dadjoke(replies) -> None:
+    """get random dad joke."""
+    headers = {
+        "User-Agent": "SimpleBot (https://github.com/simplebot-org/simplebot)",
+        "Accept": "text/plain",
+    }
+    with requests.get("https://icanhazdadjoke.com/", headers=headers) as resp:
+        replies.add(text=resp.text)
+
+
+@simplebot.command
+def flip(payload, replies) -> None:
+    """Flip given text."""
     import upsidedown
 
-    @simplebot.command
-    def flip(payload, replies) -> None:
-        """Flip given text."""
-        replies.add(text=upsidedown.transform(payload or "no text given"))
+    replies.add(text=upsidedown.transform(payload or "no text given"))
 
 
-except ImportError as ex:
-    print("WARNING:", ex)
-
-
-try:
+@simplebot.command(admin=True)
+def html2img(payload, message, replies) -> None:
     import imgkit
 
-    @simplebot.command(admin=True)
-    def html2img(payload, message, replies) -> None:
-        """html to image"""
-        options = {
-            "format": "webp",
-            "width": "370",
-            "quality": "80",
-        }
-        img = imgkit.from_url(payload, False, options=options)
-        replies.add(filename="html.webp", bytefile=io.BytesIO(img), quote=message)
-
-
-except ImportError as ex:
-    print("WARNING:", ex)
+    """html to image"""
+    options = {
+        "format": "webp",
+        "width": "370",
+        "quality": "80",
+    }
+    img = imgkit.from_url(payload, False, options=options)
+    replies.add(filename="html.webp", bytefile=io.BytesIO(img), quote=message)
 
 
 @simplebot.command(name="/echoAs")
