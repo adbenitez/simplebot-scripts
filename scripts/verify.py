ef41555c60c21bc14f2fad9d938c7c3d14ee9774
adbenitez <asieldbenitez@gmail.com>
2021-09-03 12:30:20 -0400

    add scripts/verify.py

---
+++ scripts/verify.py
@@ -0,0 +1,29 @@
+"""
+To use first install dependencies:
+    pip install "qrcode[pil]"
+"""
+
+import io
+import qrcode
+import simplebot
+from urllib.parse import unquote_plus
+from simplebot.bot import DeltaBot, Replies
+from deltachat import Message
+
+
+@simplebot.filter
+def verification_filter(message: Message) -> None:
+    """Send me your OPENPGP4FPR link and I will verify you."""
+    link = message.text.split(maxsplit=1)[0]
+    if link.lower().startswith("openpgp4fpr:") and "g=" not in link:
+        if f"a={message.get_sender_contact().addr}" in unquote_plus(link):
+            message.account.qr_setup_contact(link)
+
+
+@simplebot.command
+def verify(bot: DeltaBot, replies: Replies) -> None:
+    """Get the bot's verification QR code."""
+    buffer = io.BytesIO()
+    qrcode.make(bot.account.get_setup_contact_qr()).save(buffer, format="jpeg")
+    buffer.seek(0)
+    replies.add(filename="qr.jpg", bytefile=buffer)
