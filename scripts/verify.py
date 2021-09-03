abb8546894311a20737cfbdb39d74d64feec8515
adbenitez <asieldbenitez@gmail.com>
2021-09-03 13:12:11 -0400

    update scripts/verify.py

--- scripts/verify.py
+++ scripts/verify.py
@@ -1,6 +1,10 @@
 """
 To use first install dependencies:
     pip install "qrcode[pil]"
+    pip install pyzbar
+
+You will need to install the zbar shared library:
+    sudo apt-get install libzbar0
 """
 
 import io
@@ -9,20 +13,29 @@ from urllib.parse import unquote_plus
 import qrcode
 import simplebot
 from deltachat import Message
+from PIL import Image
+from pyzbar import pyzbar
 from simplebot.bot import DeltaBot, Replies
 
 
 @simplebot.filter
 def verification_filter(message: Message) -> None:
-    """Send me your OPENPGP4FPR link and I will verify you."""
-    link = message.text.split(maxsplit=1)[0]
-    if link.lower().startswith("openpgp4fpr:") and "g=" not in link:
-        if f"a={message.get_sender_contact().addr}" in unquote_plus(link):
-            message.account.qr_setup_contact(link)
+    """Send me your contact QR or OPENPGP4FPR link in private and I will verify you."""
+    if message.chat.is_group():
+        return
+    if message.is_image():
+        results = pyzbar.decode(Image.open(message.filename))
+        link = results[0].data.decode() if results else ""
+    else:
+        link = message.text
+    link = link.split(maxsplit=1)[0]
+    addr = message.get_sender_contact().addr
+    if link.lower().startswith("openpgp4fpr:") and f"a={addr}" in unquote_plus(link):
+        message.account.qr_setup_contact(link)
 
 
 @simplebot.command
-def verify(bot: DeltaBot, replies: Replies) -> None:
+def contactQR(bot: DeltaBot, replies: Replies) -> None:
     """Get the bot's verification QR code."""
     buffer = io.BytesIO()
     qrcode.make(bot.account.get_setup_contact_qr()).save(buffer, format="jpeg")
