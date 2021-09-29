47944920f269ac8d8f446931f8ba39798cbad9dd
adbenitez <asieldbenitez@gmail.com>
2021-09-29 03:51:31 -0400

    adapt scripts/dice_tournament.py to new simplebot_score version

--- scripts/dice_tournament.py
+++ scripts/dice_tournament.py
@@ -1,98 +1,131 @@
 import random
 import time
+from threading import Thread
+from typing import Optional
 
 import simplebot
-import simplebot_score as sc  # noqa
+from simplebot_score import _getdefault  # noqa
+from simplebot_score.orm import Base, User, session_scope  # noqa
+from sqlalchemy import Column, Float, String
 
-db: sc.DBManager
-DICES = ("⚀", "⚁", "⚂", "⚃", "⚄", "⚅")
-MINUTES = 30
+DICES = {
+    1: "⚀",
+    2: "⚁",
+    3: "⚂",
+    4: "⚃",
+    5: "⚄",
+    6: "⚅",
+}
+TAVERN_COOLDOWN = 60 * 15
+BET = 5
+
+
+class Tavern(Base):
+    addr = Column(String(500), primary_key=True)
+    join_time = Column(Float, nullable=False)
 
 
 @simplebot.hookimpl
-def deltabot_init(bot) -> None:
-    global db
-    db = sc._get_db(bot)
-    with db.db:
-        db.db.execute(
-            """CREATE TABLE IF NOT EXISTS tournament
-            (addr TEXT PRIMARY KEY,
-            time FLOAT NOT NULL)"""
-        )
+def deltabot_init(bot: simplebot.DeltaBot) -> None:
+    badge = _getdefault(bot, "score_badge", "🎖️")
+    desc = f"Probar suerte jugando dados en la taberna, para entrar debes pagar {BET}{badge}"
+
+    bot.commands.register(func=taberna, help=desc)
 
 
-@simplebot.command
+@simplebot.hookimpl
+def deltabot_start(bot: simplebot.DeltaBot) -> None:
+    Thread(target=_check_tavern, args=(bot,)).start()
+
+
 def taberna(bot, message, replies) -> None:
-    """Probar suerte jugando dados en la taberna, para entrar debes tener al menos 1🎖"""
-    addr = message.get_sender_contact().addr
-    score = db.get_score(addr)
-    if score < 1:
-        replies.add(text="❌ No tienes 🎖(puntos de honor) suficientes para jugar")
-        return
-    row = _get_opponent(addr)
-    if row:
-        p1, p2 = row["addr"], addr
-        bet = 10 if min((db.get_score(p1), db.get_score(p2))) / 2 > 10 else 1
-        roll1 = _roll_dice()
-        roll2 = _roll_dice()
-        while roll1[1] == roll2[1]:
+    badge = _getdefault(bot, "score_badge", "🎖️")
+    with session_scope() as session:
+        user2 = (
+            session.query(User)
+            .filter_by(addr=message.get_sender_contact().addr)
+            .first()
+        )
+        score = user2.score if user2 else 0
+        if score < BET:
+            replies.add(
+                text=f"❌ No tienes {badge} suficientes para entrar a la taberna",
+                quote=message,
+            )
+            return
+        user1 = _get_opponent(user2.addr, session)
+        if user1:
             roll1 = _roll_dice()
             roll2 = _roll_dice()
-        if roll1[1] < roll2[1]:
-            p1, p2 = addr, row["addr"]
-            roll1, roll2 = roll2, roll1
+            while sum(roll1) == sum(roll2):
+                roll1 = _roll_dice()
+                roll2 = _roll_dice()
+            if sum(roll1) < sum(roll2):
+                user1, user2 = user2, user1
+                roll1, roll2 = roll2, roll1
 
-        p2_score = db.get_score(p2) - bet
-        db.set_score(p2, p2_score)
-        p1_score = db.get_score(p1) + bet
-        db.set_score(p1, p1_score)
+            user1.score += BET * 2
 
-        text = "🎲 Lanzan los dados sobre la mesa:\n\n"
-        text += "{0} {1} ({2})\n{3} {4} ({5})\n\n"
-        text += "{0} ganó! y se lleva {6}🎖 de {3}"
-        text = text.format(
-            bot.get_contact(p1).name,
-            " + ".join(roll1[0]),
-            roll1[1],
-            bot.get_contact(p2).name,
-            " + ".join(roll2[0]),
-            roll2[1],
-            bet,
-        )
-        text += "\n\nTienes: {}🎖"
-        replies.add(text=text.format(p1_score), chat=bot.get_chat(p1))
-        replies.add(text=text.format(p2_score), chat=bot.get_chat(p2))
-    else:
-        with db.db:
-            db.db.execute("REPLACE INTO tournament VALUES (?,?)", (addr, time.time()))
-        replies.add(
-            text="🍺 Entras a la taberna, tomas asiento y saboreas tu bebida mientras esperas a que algún oponente aparezca para jugar dados en los próximos {} minutos".format(
-                MINUTES
-            ),
-            chat=bot.get_chat(addr),
-        )
-
-
-def _roll_dice():
-    dices = []
-    total = 0
-    for _ in range(2):
-        rand = random.randrange(0, 6)
-        total += rand + 1
-        dices.append(DICES[rand])
-    return (dices, total)
+            text = "🎲 Lanzan los dados sobre la mesa:\n\n"
+            text += "{0} {1} ({2})\n{3} {4} ({5})\n\n"
+            text += "{0} ganó! y se lleva {6}{7}"
+            text = text.format(
+                bot.get_contact(user1.addr).name,
+                " + ".join(DICES[val] for val in roll1),
+                sum(roll1),
+                bot.get_contact(user2.addr).name,
+                " + ".join(DICES[val] for val in roll2),
+                sum(roll2),
+                BET * 2,
+                badge,
+            )
+            text += "\n\nTienes: "
+            replies.add(
+                text=f"{text}{user1.score}{badge}", chat=bot.get_chat(user1.addr)
+            )
+            replies.add(
+                text=f"{text}{user2.score}{badge}", chat=bot.get_chat(user2.addr)
+            )
+        else:
+            chat = bot.get_chat(user2.addr)
+            if session.query(Tavern).filter_by(addr=user2.addr).first():
+                replies.add(text="❌ Ya estás en la taberna", chat=chat)
+            else:
+                user2.score -= BET
+                player = Tavern(addr=user2.addr, join_time=time.time())
+                session.add(player)
+                replies.add(
+                    text=f"🍺 Entras a la taberna, tomas asiento y saboreas tu bebida mientras esperas en los próximos {TAVERN_COOLDOWN // 60} minutos a que algún oponente aparezca para jugar dados",
+                    chat=chat,
+                )
 
 
-def _delete_player(addr):
-    with db.db:
-        db.db.execute("DELETE FROM tournament WHERE addr=?", (addr,))
+def _roll_dice(n=2) -> tuple:
+    return tuple(random.randint(1, 6) for _ in range(n))
 
 
-def _get_opponent(addr):
-    q = "SELECT * FROM tournament WHERE addr!=? ORDER BY time DESC"
-    for row in db.db.execute(q, (addr,)):
-        _delete_player(row["addr"])
-        if time.time() - row["time"] > 60 * MINUTES:
+def _get_opponent(addr: str, session) -> Optional[User]:
+    for player in session.query(Tavern):
+        if player.addr == addr:
             continue
-        _delete_player(addr)
-        return row
+        session.delete(player)
+        return session.query(User).filter_by(addr=player.addr).one()
+    return None
+
+
+def _check_tavern(bot: simplebot.DeltaBot) -> None:
+    badge = _getdefault(bot, "score_badge", "🎖️")
+    while True:
+        try:
+            time.sleep(10)
+            with session_scope() as session:
+                for player in session.query(Tavern):
+                    if time.time() - player.join_time > TAVERN_COOLDOWN:
+                        user = session.query(User).filter_by(addr=player.addr).one()
+                        session.delete(player)
+                        user.score += BET
+                        bot.get_chat(user.addr).send_text(
+                            f"+{BET}{badge} Sales de la taberna sin que nada interesante pase."
+                        )
+        except Exception as err:
+            bot.logger.exception(err)
